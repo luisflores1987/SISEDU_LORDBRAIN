2f01177b10bd2481011779cb526282e4f0acd314
Luis Flores <lafrgnr4@gmail.com>
2024-04-25 08:43:23 -0500

    SERVIDOR: se agrega que actualice despues de buscar al alumno y setee el numero de recibo | master

--- web/js/registroPagos.js
+++ web/js/registroPagos.js
@@ -238,6 +238,7 @@ function buscarPagoBuscarPagosAlumno(numeroDocumento) {
 
                         fnHabilitarTextBoxBuscarPagosAlumno(nIdAlumnoDeuda);
                         fnCancelarBoletasBuscarPagosAlumno(nIdAlumnoDeuda);
+                        obtenerNumeroRecibo();
                     }
 
                     fnIngresoBoletaBuscarPagosAlumno();
